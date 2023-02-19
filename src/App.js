4aa433f2f17a0d78ffa0863804d634cae671df59
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-02-19 20:13:18 +0100

    started the user emergency page function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Emergency from './Components/Emergency';
 
 function App() {
   return (
     <>
       <h1>Welcome to Emergency System</h1>
+      <Emergency />
     </>
   );
 }
