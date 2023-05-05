03cc90cf7bb5abcfd22445fbd3149a692a0ebb4b
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-05-05 16:36:05 +0100

    updated env

--- src/URL.js
+++ src/URL.js
@@ -1,7 +1,7 @@
  const baseUrl = process.env.NODE_ENV === 'development'? process.env.REACT_APP_SERVER_DEV_URL: process.env.REACT_APP_SERVER_PRO_URL;
 
  const locationURI =(latitude, longitude)=>{
-    const URI = `${process.env.REACT_APP_GEOAPIFY_URL}=${latitude}&lon=${longitude}&apiKey=${process.env.REACT_APP_API_KEY}
+    const URI = `${process.env.REACT_APP_GEOAPIFY_URL}=${latitude}&lon=${longitude}&apiKey=${process.env.REACT_APP_GEOPIFY_API_KEY}
     `
     return URI
  }
