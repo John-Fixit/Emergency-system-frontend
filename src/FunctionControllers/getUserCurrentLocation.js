a3392c227c2ed56451fd6d5802bc3e963e74fe7b
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-06-15 07:14:38 +0100

    updated

--- src/FunctionControllers/getUserCurrentLocation.js
+++ src/FunctionControllers/getUserCurrentLocation.js
@@ -3,9 +3,11 @@ import { locationURI } from "../URL";
 
 const getLocation = async (latitude, longitude) => {
   const URI = locationURI(latitude, longitude);
+
   let resData = await axios.get(URI);
-  let data = await resData.data.features[0].properties;
-  return data;
+  // let data = await resData.data.features[0].properties;
+  let data = await resData?.data?.results[0];
+  return data; 
 };
 
 export { getLocation };
--- src/URL.js
+++ src/URL.js
@@ -1,9 +1,16 @@
  const baseUrl = process.env.NODE_ENV === 'development'? process.env.REACT_APP_SERVER_DEV_URL: process.env.REACT_APP_SERVER_PRO_URL;
 
+//  const locationURI =(latitude, longitude)=>{
+//     const URI = `${process.env.REACT_APP_GEOAPIFY_URL}=${latitude}&lon=${longitude}&apiKey=${process.env.REACT_APP_GEOPIFY_API_KEY}
+//     `
+//     return URI
+//  }
  const locationURI =(latitude, longitude)=>{
-    const URI = `${process.env.REACT_APP_GEOAPIFY_URL}=${latitude}&lon=${longitude}&apiKey=${process.env.REACT_APP_GEOPIFY_API_KEY}
+    const URI = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.REACT_APP_OPENCAGE_API_KEY}
     `
     return URI
  }
+
+
 
  export {baseUrl, locationURI}
